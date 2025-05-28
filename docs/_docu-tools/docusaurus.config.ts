24719bbee6d48c0fb8c4d349c474d120c6026b46
piinthecloud <stephanie.pi@automattic.com>
2025-05-28 17:48:19 +0200

    update config

--- docs/_docu-tools/docusaurus.config.ts
+++ docs/_docu-tools/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
-	url: 'https://developer.woocommerce.com',
+	url: 'https://woocommmerce.github.io',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: '/',
+	baseUrl: '/docs/',
 
 	// GitHub pages deployment config.
 	// If you aren't using GitHub pages, you don't need these.
